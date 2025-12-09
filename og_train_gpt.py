b903d7b90bb9fbc7e4127fa4fcdb829e92db63b7
James <jsteeman@seas.upenn.edu>
2025-12-09 15:28:49 -0500

    try to fix syntax in og part 2

--- og_train_gpt.py
+++ og_train_gpt.py
@@ -1,5 +1,5 @@
 # NOTE: record from https://github.com/KellerJordan/modded-nanogpt/blob/master/records/track_1_short/2024-10-14_ModernArch/dabaaddd-237c-4ec9-939d-6608a9ed5e27.txt
-====================================================================================================
+#====================================================================================================
 import os
 import sys
 with open(sys.argv[0]) as f:
@@ -26,15 +26,15 @@ def zeropower_via_svd(G, steps=None):
 
 @torch.compile
 def zeropower_via_newtonschulz5(G, steps=10, eps=1e-7):
-    """
-    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
-    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
-    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
-    zero even beyond the point where the iteration no longer converges all the way to one everywhere
-    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
-    where S' is diagonal with S_{ii}' \sim Uniform(0.5, 1.5), which turns out not to hurt model
-    performance at all relative to UV^T, where USV^T = G is the SVD.
-    """
+    #"""
+    #Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+    #quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+    #of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+    #zero even beyond the point where the iteration no longer converges all the way to one everywhere
+    #on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+    #where S' is diagonal with S_{ii}' \sim Uniform(0.5, 1.5), which turns out not to hurt model
+    #performance at all relative to UV^T, where USV^T = G is the SVD.
+    #"""
     assert len(G.shape) == 2
     a, b, c = (3.4445, -4.7750,  2.0315)
     X = G.bfloat16()
@@ -52,30 +52,30 @@ def zeropower_via_newtonschulz5(G, steps=10, eps=1e-7):
 zeropower_backends = dict(svd=zeropower_via_svd, newtonschulz5=zeropower_via_newtonschulz5)
 
 class Muon(torch.optim.Optimizer):
-    """
-    Muon - MomentUm Orthogonalized by Newton-schulz
+    #"""
+    #Muon - MomentUm Orthogonalized by Newton-schulz
 
-    Muon internally runs standard SGD-momentum, and then performs an orthogonalization post-
-    processing step, in which each 2D parameter's update is replaced with the nearest orthogonal
-    matrix. To efficiently orthogonalize each update, we use a Newton-Schulz iteration, which has
-    the advantage that it can be stably run in bfloat16 on the GPU.
+    #Muon internally runs standard SGD-momentum, and then performs an orthogonalization post-
+    #processing step, in which each 2D parameter's update is replaced with the nearest orthogonal
+    #matrix. To efficiently orthogonalize each update, we use a Newton-Schulz iteration, which has
+    #the advantage that it can be stably run in bfloat16 on the GPU.
 
-    Some warnings:
-    - This optimizer assumes that all parameters passed in are 2D.
-    - It should not be used for the embedding layer, the final fully connected layer, or any {0,1}-D
-    parameters; those should all be optimized by a standard method (e.g., AdamW).
-    - To use it with 4D convolutional filters, it works well to just flatten their last 3 dimensions.
-    - We believe it is unlikely to work well for training with small batch size.
-    - We believe it may not work well for finetuning pretrained models, but we haven't tested this.
-    - We have not yet tried this optimizer for training scenarios larger than NanoGPT (124M).
+    #Some warnings:
+    #- This optimizer assumes that all parameters passed in are 2D.
+    #- It should not be used for the embedding layer, the final fully connected layer, or any {0,1}-D
+    #parameters; those should all be optimized by a standard method (e.g., AdamW).
+    #- To use it with 4D convolutional filters, it works well to just flatten their last 3 dimensions.
+    #- We believe it is unlikely to work well for training with small batch size.
+    #- We believe it may not work well for finetuning pretrained models, but we haven't tested this.
+    #- We have not yet tried this optimizer for training scenarios larger than NanoGPT (124M).
 
-    Arguments:
-        lr: The learning rate used by the internal SGD.
-        momentum: The momentum used by the internal SGD.
-        nesterov: Whether to use Nesterov-style momentum in the internal SGD. (recommended)
-        backend: The chosen backend for the orthogonalization step. (recommended: 'newtonschulz5')
-        backend_steps: The number of iteration steps to use in the backend, if it is iterative.
-    """
+    #Arguments:
+    #    lr: The learning rate used by the internal SGD.
+    #    momentum: The momentum used by the internal SGD.
+    #    nesterov: Whether to use Nesterov-style momentum in the internal SGD. (recommended)
+    #    backend: The chosen backend for the orthogonalization step. (recommended: 'newtonschulz5')
+    #    backend_steps: The number of iteration steps to use in the backend, if it is iterative.
+    #"""
     def __init__(self, params, lr=3e-4, momentum=0.95, nesterov=True, backend='newtonschulz5', backend_steps=5):
         defaults = dict(lr=lr, momentum=momentum, nesterov=nesterov, backend=backend, backend_steps=backend_steps)
         super().__init__(params, defaults)
